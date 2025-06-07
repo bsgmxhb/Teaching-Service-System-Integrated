c63e0e5e477c6113ff3323ef18a0e8112396a8a2
bsgmxhb <3230106036@zju.edu.cn>
2025-06-07 11:35:19 +0800

    Update HomeworkTeacher.vue

--- src/pages/resource_share/teacher/HomeworkTeacher.vue
+++ src/pages/resource_share/teacher/HomeworkTeacher.vue
@@ -174,7 +174,7 @@
 <script lang="ts" setup>
 import { ref, reactive, onMounted, computed } from 'vue'
 import { ElMessage } from 'element-plus'
-import { teacherAPI } from '../../api/teacher'
+import { teacherAPI } from '../../../api/resource_share/teacher'
 
 const allCourses = ref<any[]>([])
 const courseHomeworkList = ref<any[]>([])
